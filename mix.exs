9c652a864f4bef25d7e7fc0a863d7077bcc359ac
Christian Briones <cwbriones@gmail.com>
2016-04-14 21:02:47 -0700

    update package data

--- mix.exs
+++ mix.exs
@@ -5,28 +5,25 @@ defmodule Scientist.Mixfile do
     [app: :scientist,
      version: "0.1.0",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package,
+     name: "Scientist",
+     source_url: "https://github.com/cwbriones/scientist",
+     description: """
+     An elixir library for refactoring critical paths in your application.
+     """]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    []
+  defp deps, do: []
+
+  defp package do
+    [maintainers: ["Christian Briones"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/cwbriones/scientist"},
+     files: ~w(lib LICENSE mix.exs README.md)]
   end
 end
